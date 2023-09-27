fa59848e64aa68d49df4cfec250e23fa59f24bc7
kimi <kimi.lai@tpisoftware.com>
2023-09-27 14:00:29 +0800

    chore: 調整環境

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // fileTransfer: FileTransferObject = this.transfer.create();
 
-  constructor() {}
+  constructor(
+    //private transfer: FileTransfer,
 
+    private file: File
+  ) {}
+
+  clickButton(): void {
+    console.log('click');
+  }
+
+  // upload() {
+  //   let options: FileUploadOptions = {
+  //     fileKey: 'file',
+  //     fileName: 'name.jpg', // 文件類型
+  //     headers: {},
+  //     params: {}, // 如果要傳參數，寫這裡
+  //   };
+
+  //   // URL
+  //   let url = 'https://example.com/upload';
+
+  //   // File path
+  //   let filePath = 'path/to/your/file.jpg';
+
+  //   this.fileTransfer.upload(filePath, url, options).then(
+  //     (data) => {
+  //       // Success! The file has been uploaded.
+  //       console.log('File upload success:', data);
+  //     },
+  //     (err) => {
+  //       // Handle error
+  //       console.error('File upload error:', err);
+  //     }
+  //   );
+  // }
+
+  // download() {
+  //   const url = 'http://www.example.com/file.pdf';
+  //   this.fileTransfer.download(url, this.file.dataDirectory + 'file.pdf').then(
+  //     (entry) => {
+  //       console.log('download complete: ' + entry.toURL());
+  //     },
+  //     (error) => {
+  //       // handle error
+  //     }
+  //   );
+  // }
 }
